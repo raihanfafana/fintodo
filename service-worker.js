e9f4a42d87f3b6a0d98648e6475ca0d435e4faff
raihanfafana <raihanfafana@gmail.com>
2026-01-02 02:25:59 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,53 @@
-const CACHE_NAME = 'fintodo-v2';
+const CACHE_NAME = 'fintodo-v4';
 
 const ASSETS = [
-  '/',
+  '/fintodo/',
   '/fintodo/index.html',
+  '/fintodo/manifest.json',
   '/fintodo/css/style.css',
   '/fintodo/js/app.js',
   '/fintodo/logo_app.png'
 ];
 
+// INSTALL
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// ACTIVATE
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH (cache-first, fallback network)
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).then(fetchRes => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
+    })
   );
 });
