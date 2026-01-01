ee678d23837ad87c97d1227e7e26135e05518f00
raihanfafana <raihanfafana@gmail.com>
2026-01-01 20:03:14 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,15 @@
-const CACHE_NAME = "fintodo-cache-v3";
-const FILES_TO_CACHE = [
-  "/fintodo/",
-  "/fintodo/index.html",
-  "/fintodo/manifest.json",
-  "/fintodo/css/style.css",
-  "/fintodo/js/app.js"
+const CACHE_NAME = 'fintodo-v2';
+
+const ASSETS = [
+  '/',
+  '/fintodo/index.html',
+  '/fintodo/css/style.css',
+  '/fintodo/js/app.js',
+  '/fintodo/logo_app.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request).catch(() => {
-          return caches.match("/fintodo/index.html");
-        });
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
